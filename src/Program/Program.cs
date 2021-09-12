b70b3785ba3d8b8e374355574a3501c72ed4bf5a
martino-lucas95 <martino.lucas95@gmail.com>
2021-09-12 17:31:30 -0300

    Adding Tests

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,8 +8,10 @@ namespace UnitTestAndDebug
         {
             Person john = new Person("John Doe", "1.234.567-8");
             Person jane = new Person("Jane Doe", "8.765.432-1");
+            Person Lucas = new Person("Lucas Martino", "4.658.761-8");
             john.IntroduceYourself();
             jane.IntroduceYourself();
+            Lucas.IntroduceYourself();
         }
     }
 }
--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -1,21 +1,50 @@
 using NUnit.Framework;
-
 using UnitTestAndDebug;
 
 namespace Tests
 {
+    [TestFixture]
     public class Tests
     {
+        private Person person;
+
         [SetUp]
         public void Setup()
         {
-            // Insertá tu código de inicialización aquí
+            this.person = new Person("John Doe", "1.234.567-8");
         }
 
         [Test]
-        public void TestValidCI() // Cambiá el nombre para indicar qué estás probando
+        public void NameNotNullOrEmpty()
         {
+            //Not Null
+            Assert.IsNotNull(person.Name);
+            //Not Empty   
+            person.Name= "";
+            Assert.IsNotEmpty(person.Name);         
+        }
+
+        [Test]
+        public void ChangeName() 
+        {
+            person.Name = "Lucas";
+            Assert.AreEqual("Lucas", person.Name);
+        }
+
+        [Test]
+        public void IDNotEmpty() 
+        {
+            person.ID = "";
+            Assert.IsNotEmpty(person.ID);
+        }
+
+        [Test]
+        public void CorrectLastIDDigit()
+        {   
+            person.ID = "46587618";
+            Assert.IsTrue(IdUtils.IdIsValid(person.ID));
             
         }
+
     }
 }
